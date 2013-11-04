e4bd7fedeec58c60c1cb231fa688d2fa1b998c64
OpenZIS <Hello@openzis.org>
2013-11-04 15:48:27 -0500

    update 2
    Update 2

---
+++ OpenZIS/db/mysql/events.sql
@@ -0,0 +1,35 @@
+SHOW PROCESSLIST;
+
+
+SET GLOBAL event_scheduler = ON;
+SET @@global.event_scheduler = ON;
+SET GLOBAL event_scheduler = 1;
+SET @@global.event_scheduler = 1;
+
+
+DROP TABLE IF EXISTS `agent_filters`;
+
+
+DROP EVENT IF EXISTS clearmessagequeue;
+
+
+CREATE EVENT clearmessagequeue
+    ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 MINUTE
+    DO
+      delete from ``messagequeue` where status_id = 3;
+	
+
+DROP EVENT IF EXISTS `clearerrorlog`;
+
+CREATE EVENT `clearerrorlog`
+    ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 7 DAY
+    DO
+      delete from `error_log`;
+	
+	
+DROP EVENT IF EXISTS `archivezitlog`;
+
+CREATE EVENT `archivezitlog`
+    ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 DAY
+    DO
+      update `zit_log` set archived = 1 where archived = 0;
